54aeb39833b9985378d08fb2720979269140cba4
estebanpablo89 <estebanpablo89@gmail.com>
2020-11-21 19:23:21 -0500

    auth

--- middleware/error.js
+++ middleware/error.js
@@ -16,7 +16,9 @@ const errorHandler = (err, req, res, next) => {
 
   // Mongoose duplicate key
   if (err.code === 11000) {
-    const message = 'Duplicate field value entered';
+    const message = `Duplicate ${
+      Object.keys(err.keyValue)[0]
+    } field value entered`;
     error = new ErrorResponse(message, 400);
   }
 
