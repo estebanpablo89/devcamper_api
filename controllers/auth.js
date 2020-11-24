76dc9d33389accd7aa89fb984696f1e7342dc5c7
estebanpablo89 <estebanpablo89@gmail.com>
2020-11-24 17:31:21 -0500

    security

--- controllers/auth.js
+++ controllers/auth.js
@@ -98,6 +98,22 @@ exports.updatePassword = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc    Log user out / clear cookie
+// @route   GET /api/auth/logout
+// @access  Private
+
+exports.logOut = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 + 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
+
 // @desc    Get current logged in user
 // @route   GET /api/auth/me
 // @access  Private
